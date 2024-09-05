047cabd42b313836387105ba21503c2956d54aed
Vivekshrotriya1 <vivekshrotriyas123@gmail.com>
2024-09-06 03:42:22 +0530

    add

--- client/src/Components/Login/vivek.js
+++ client/src/Components/Login/vivek.js
@@ -1,12 +1,12 @@
 import { Card, CardBody, CardImg, CardTitle, Col } from "reactstrap";
-import admin from "../../assets/admin.png";
-// import Doctor from "../../assets/doctor_new.png";
+// import admin from "../../assets/admin.png";
+import Doctor from "../../assets/doctor_new.png";
 export default function LoginCard({ role }) {
   return (
     <Col sm="12" md="4">
       <Card style={{ "box-shadow": "4px 4px 4px 4px #CCCCCC" }}>
-        <CardImg top width="10%" src={admin} alt="Card image cap" />
-        {/* <CardImg top width="10%" src={Doctor} alt="Card image cap" /> */}
+        {/* <CardImg top width="10%" src={admin} alt="Card image cap" /> */}
+        <CardImg top width="10%" src={Doctor} alt="Card image cap" />
         <CardBody>
           <CardTitle tag="h1">Hello!! {role.toUpperCase()}</CardTitle>
         </CardBody>
